3ac677027ce70bb164e01752dea1b2a8028abca4
baselsamy <30785124+baselsamy@users.noreply.github.com>
2026-01-10 01:55:26 +0200

    Update base path after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/nexus-portal/",
+  base: "/basel-samy/",
   server: {
     host: "::",
     port: 8080,
